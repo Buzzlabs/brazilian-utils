2b1a847f4555a7a85c369ebd439096ec44550903
Ana Luiza Bastos <hello@anabastos.me>
2026-01-29 19:50:58 -0300

    chore: configure for Clojars publication with full documentation setup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,24 @@
 # Changelog
 
-Todas as mudanças notáveis neste projeto serão documentadas neste arquivo.
+All notable changes to this project will be documented in this file.
 
-O formato é baseado em [Keep a Changelog](https://keepachangelog.com/pt-BR/1.0.0/),
-e este projeto adere a [Semantic Versioning](https://semver.org/lang/pt-BR/).
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.1.0](https://github.com/seu-usuario/brazilian-utils/releases/tag/0.1.0) (2026-01-05)
+## [Unreleased]
 
-### Features
+## [0.1.0] - 2026-01-29
+
+### Added
+- Initial release
+- CPF validation and formatting
+- CNPJ validation and formatting
+- CEP validation and formatting
+- Phone number validation
+- States and cities utilities
+- Boleto validation
+- Support for both Clojure and ClojureScript
+- Comprehensive documentation
+
+[Unreleased]: https://github.com/Buzzlabs/brazilian-utils/compare/0.1.0...HEAD
+[0.1.0]: https://github.com/Buzzlabs/brazilian-utils/releases/tag/0.1.0
---
+++ project.clj
@@ -0,0 +1,41 @@
+(defproject com.github.Buzzlabs/brazilian-utils "0.1.0"
+  :description "Utils library for Brazilian-specific businesses"
+  :url "https://github.com/Buzzlabs/brazilian-utils"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  
+  :plugins [[lein-codox "0.10.8"]]
+  
+  :codox {:output-path "doc/api"
+          :source-paths ["src"]
+          :doc-files ["README.MD" 
+                      "CHANGELOG.md"
+                      "docs/index.md"
+                      "docs/guides/usage.md"
+                      "docs/guides/installation.md"
+                      "docs/modules/boleto.md"
+                      "docs/modules/capitalize.md"
+                      "docs/modules/cep.md"
+                      "docs/modules/cities.md"
+                      "docs/modules/cnh.md"
+                      "docs/modules/cnpj.md"
+                      "docs/modules/cpf.md"
+                      "docs/modules/currency.md"
+                      "docs/modules/email.md"
+                      "docs/modules/inscricao_estadual.md"
+                      "docs/modules/license_plate.md"
+                      "docs/modules/phone.md"
+                      "docs/modules/pis.md"
+                      "docs/modules/processo_juridico.md"
+                      "docs/modules/renavam.md"
+                      "docs/modules/states.md"
+                      "docs/modules/titulo_eleitoral.md"]
+          :source-uri "https://github.com/Buzzlabs/brazilian-utils/blob/{version}/{filepath}#L{line}"
+          :metadata {:doc/format :markdown}}
+  
+  :deploy-repositories {"clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases false}})
